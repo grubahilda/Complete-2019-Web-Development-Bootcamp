5742e81bac04538cc10c84c8ac0322172de3c41f
grubahilda <marta.pozaroszczyk@planipy.com>
2019-06-10 13:49:47 +0200

    Add Wik-API with get/post/delete requests for articles and database

---
+++ Wiki-API/server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const Pool = require('pg').Pool;
+const ejs = require('ejs');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: "wikidb",
+    password: "pass",
+    port: 5432
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+
+
+app.route("/articles")
+    .get((_req, res) => {
+
+        pool.query('SELECT * FROM articles', (error, results) => {
+            if (error) {
+                console.log(error);
+                throw error
+            }
+
+            res.send(results.rows);
+
+        })
+    })
+    .post((req, res) => {
+
+        pool.query('INSERT INTO articles (title, content) VALUES ($1, $2)', [req.body.title, req.body.content], (error, result) => {
+            if (error) {
+                throw error
+            }
+            res.sendStatus(200);
+        });
+
+
+    })
+    .delete((_req, _res) => {
+        pool.query('DELETE FROM articles', (error, _results) => {
+            if (error) {
+                throw error
+            }
+
+        })
+    });
+
+
+app.listen(3000, function (err, _res) {
+    if (err) {
+        console.log("Error: " + err);
+    } else {
+        console.log("Server running on port 3000");
+    }
+
+
+});
